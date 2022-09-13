6120019c2934766729b75c48e0af198d7a3631c5
Vladislav Kuznetsov <smithy-vl@yandex.ru>
2022-09-13 16:29:31 +0300

    Создание структуры проекта, добавление начального оглавления и разбор одного вопроса

---
+++ core/question1/sample-java/src/main/java/io/github/smithyvl/interview/lesson1/sample/Lesson1.java
@@ -0,0 +1,9 @@
+package io.github.smithyvl.interview.lesson1.sample;
+
+import static java.lang.System.out;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        out.println("Hello world!");
+    }
+}
---
+++ core/question1/sample-kotlin/src/main/kotlin/io/github/smithyvl/interview/lesson1/sample/Lesson1.kt
@@ -0,0 +1,6 @@
+package io.github.smithyvl.interview.lesson1.sample
+
+fun main() {
+    val name = "Vlad"
+    println("Hello ${name}!")
+}
